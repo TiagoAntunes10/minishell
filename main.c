207b93fc30157f2db7fc1377a84f4364e62a2d92
Tiago <tfa.antunes@gmail.com>
2024-11-02 22:02:01 +0000

    New TODO tasks added

--- main.c
+++ main.c
@@ -6,14 +6,16 @@
 /*   By: tialbert <tialbert@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/12 10:50:12 by tialbert          #+#    #+#             */
-/*   Updated: 2024/10/17 21:46:54 by tialbert         ###   ########.fr       */
+/*   Updated: 2024/11/02 17:00:54 by tialbert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Include/minishell.h"
 
 //TODO: Verify if fd 1 is a terminal (that should be the standard)
-int	main(void)
+//TODO: Check if this is the best way to have access to the envirenment variables
+//TODO: cd must execute in the parent process (it does not work with pipes and ';'), all other cmds execute in child processes
+int	main(int argc, char **argv, char **env)
 {
 	char		*input;
 	t_tokens	*tokens_lst;
