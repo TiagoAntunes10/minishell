c8e41975cd64bee941bb51960994ac17f80c32b7
Rapcampo <raphael.campospt@gmail.com>
2024-11-08 11:24:08 +0000

    work in progress

--- srcs/signals/signal_sorting.c
+++ srcs/signals/signal_sorting.c
@@ -1,19 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   handler.c                                          :+:      :+:    :+:   */
+/*   signal_sorting.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rapcampo <rapcampo@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/31 19:03:38 by rapcampo          #+#    #+#             */
-/*   Updated: 2024/10/31 22:48:37 by rapcampo         ###   ########.fr       */
+/*   Updated: 2024/11/08 11:23:34 by rapcampo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	signal_sort(void)
+void	signal_parent(void)
 {
 	signal(SIGINT, handle_sigint);
+	signal(SIGQUIT, SIG_IGN);
+}
+
+void	signal_child(void)
+{
+	signal(SIGINT, handle_child);
+	signal(SIGQUIT, handle_child);
+}
+
+void	signal_ignore(void)
+{
+	signal(SIGINT, SIG_IGN);
 	signal(SIGQUIT, SIG_IGN);
 }
