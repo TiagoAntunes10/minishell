0cff63fe82457c7a075129eee053a129b30cc725
Raphael Vieira <142260510+Rapcampo@users.noreply.github.com>
2024-11-09 15:08:38 +0000

    Add files via upload

---
+++ Include/macro.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   colours.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rapcampo <rapcampo@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/31 17:13:57 by rapcampo          #+#    #+#             */
+/*   Updated: 2024/10/31 17:14:57 by rapcampo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef COLOURS_H
+# define COLOURS_H
+
+#include "minishell.h"
+
+//colour prints
+
+# define RESET "\e[0m"
+# define ULINE "\e[4m"
+# define BLINK "\e[5m"
+# define BLACK "\e[1;30m"
+# define RED "\e[1;31m"
+# define GREEN "\e[1;32m"
+# define YELLOW "\e[1;33m"
+# define BLUE  "\e[1;34m"
+# define PURPLE "\e[1;35m"
+# define CYAN "\e[1;36m"
+# define WHITE "\e[1;37m"
+
+#endif
