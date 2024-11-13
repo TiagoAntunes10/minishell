1be3d6c86bcbddd2b401826dca8dd6b1dfd617ee
Rapcampo <raphael.campospt@gmail.com>
2024-11-13 22:03:10 +0000

    header files werent updated

--- Include/minishell.h
+++ Include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: tialbert <tialbert@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/12 22:18:54 by tialbert          #+#    #+#             */
-/*   Updated: 2024/10/31 23:18:09 by rapcampo         ###   ########.fr       */
+/*   Updated: 2024/11/12 22:22:14 by tialbert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,65 +24,33 @@
 # include <errno.h>
 # include <readline/history.h>
 # include <readline/readline.h>
-# include <signal.h>
-# include "macro.h"
-# include "libft.h"
-# include "execution.h"
+# include "../libft/ft_printf.h"
+# include "tree.h"
+# include "parser.h"
+# include "cleanup.h"
 
-typedef struct s_tokens {
-	char			*infile;
-	char			*cmd;
-	char			**options;
-	char			*outfile;
-	int				priority;
-	struct s_tokens	*next;
-	struct s_tokens	*prev;
-}				t_tokens;
+# define PIPE	1
+# define LIST	2
+# define DELIM	3
+# define CMD	4
+# define REDIR	5
 
 /* ************************************************************************** */
-/*                            LIST OPERATIONS                                 */
+/*                            EXECUTION                                       */
 /* ************************************************************************** */
 
-t_tokens		*create_lst(char **tokens);
-int				lst_len(t_tokens *lst);
-
-/* ************************************************************************** */
-/*                            TOKEN CREATION                                  */
-/* ************************************************************************** */
-
-t_tokens		*tokenisation(char *input);
-
-/* ************************************************************************** */
-/*                            TOKEN UTILS                                     */
-/* ************************************************************************** */
-
-int				count_words(char *str);
-unsigned int	mod_strlen(char *str);
-char			**split_input(char *str);
-
-
-/* ************************************************************************** */
-/*                            CLEANUP                                         */
-/* ************************************************************************** */
-char			**clear_arr(char **arr);
-t_tokens		*clear_lst(t_tokens *tokens);
-void			exit_success(t_tokens *tokens, int fd);
-void			exit_failure(t_tokens *tokens, int fd);
-
-/* ************************************************************************** */
-/*                            FT_CMDS                                         */
-/* ************************************************************************** */
-void			ft_echo(t_tokens *token_lst, int *pipe);
-void			ft_pwd(int *pipe);
-void			ft_cd(t_tokens *token_lst);
-//placeholder
-void			ft_env(t_tokens *token_lst, int *pipe);
-void			ft_unset(t_tokens *token_lst, int *pipe);
-void			ft_export(t_tokens *token_lst, int *pipe);
+void	execution(t_tree *tree, int fd);
+void	exec_pipe(t_tree *tree, int fd);
+void	exec_delim(t_tree *tree, int fd);
+void	exec_list(t_tree *tree, int fd);
+void	exec_redir(t_tree *tree, int fd);
+// void			ft_echo(t_tokens *token_lst, int *pipe);
+// void			ft_pwd(int *pipe);
+// void			ft_cd(t_tokens *token_lst);
 
 /* ************************************************************************** */
 /*                            CMD UTILS                                       */
 /* ************************************************************************** */
-void			dollar_sub(t_tokens *token_lst, int type);
+// void			dollar_sub(t_tokens *token_lst, int type);
 
 #endif
