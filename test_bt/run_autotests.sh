5e66006984c4c7a1499ce51eb8593d577d60e98b
Tiago <tfa.antunes@gmail.com>
2024-12-26 12:13:01 +0000

    Testing some things with cat|cat|ls

--- test_bt/run_autotests.sh
+++ test_bt/run_autotests.sh
@@ -1,5 +1,5 @@
+#!/bin/bash/
 set -x
-#!bin/bash
 
 test_file="inputs.txt"
 
--- test_bt/run_manual.sh
+++ test_bt/run_manual.sh
@@ -1,4 +1,4 @@
-set -x
 #!/bin/bash/
 
+set -x
 valgrind -q --leak-check=full --show-leak-kinds=all --suppressions=readline_supression --track-fds=yes ./testshell
